a04ba9477c45b4ea11c1d53d425102823d39788f
Michael Salvati <print@3daddict.com>
2018-09-24 18:31:11 -0700

    setup css and flexbox skeleton in app.js

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import '../assets/css/styles.css';
 import '../assets/css/app.css';
-import logo from '../assets/images/logo.svg';
 
 const App = () => (
-    <div>
-        <div className="app">
-            <img src={logo} className="logo rotate"/>
-            <h1>Welcome to React</h1>
+    <div className="app">
+        <nav className="col-1 side-nav">
+            Nav
+        </nav>
+        <div className="col-2">
+            <header>
+                Header
+            </header>
+            <main className="content">
+            <section>
+                <h1>SOME MAIN SECTION STUFF HERE</h1>
+            </section>
+            </main>
+            <footer>
+                Footer
+            </footer>
         </div>
     </div>
 );
