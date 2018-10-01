e5d2a3f79fff7b7e4138630320b5d95c03904425
Gerry Blackmon <blackmongerry@gmail.com>
2018-10-01 16:14:58 -0700

    changed the package.json file to match the live server

--- webserver.js
+++ webserver.js
@@ -1,7 +1,7 @@
 const express = require( 'express' );
 const mySQL = require( 'mysql' );
 const server = express();
-const PORT = 3000;
+const PORT = 3050;
 const mysqlCredentials = require( './mySQLCredentials.js' );
 
 const connection = mySQL.createConnection( mysqlCredentials );
@@ -11,6 +11,11 @@ connection.connect( error => {
 	console.log( `connected to ${mysqlCredentials.database}` );
 });
 
+server.use( ( request, response, next ) => {
+	response.header( "Access-Control-Allow-Origin", "*" );
+	response.header( "Access-Control-Allow-Headers", "Origin, X-Request-With, Content-Type, Accept" );
+	next();
+});
 server.use( express.json() );
 server.use( express.urlencoded() );
 server.use( express.static( `${__dirname}/client/dist` ) );
