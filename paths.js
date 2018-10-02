d2ea65e737ce7395360b182f0e409e835602b360
Gerry Blackmon <blackmongerry@gmail.com>
2018-10-01 20:11:12 -0700

    added list insert, delete, update

--- paths.js
+++ paths.js
@@ -52,7 +52,7 @@ const paths = ( server, mySQL, connection ) => {
 		const itemUpdateInserts = [ 'name', name, 'listID', listID, 'assignedUserID', assignedUserID, 'ID', ID ];
 		const itemUpdateSQL = mySQL.format( itemUpdateQuery, itemUpdateInserts );
 
-		connection.query( itemUpdateSQL, ( error, resuls, fields ) => {
+		connection.query( itemUpdateSQL, ( error, results, fields ) => {
 			if( error ) return next( error );
 			const successString = `The item ${ID} has been updated to ${name}, ${assignedUserID}`;
 			console.log( successString );
@@ -62,10 +62,84 @@ const paths = ( server, mySQL, connection ) => {
 				data: successString
 			};
 			response.json( dataToReturn );
-		})
+		});
 	});
+	server.delete( '/api/deleteitem', ( request, response ) => {
+		const { ID } = request.body;
 
+		const itemDeleteQuery = 'UPDATE items SET ?? = ? WHERE ?? = ?';
+		const itemDeleteInserts = [ 'status', 'inactive', 'ID', ID ];
+		const itemDeleteSQL = mySQL.format( itemDeleteQuery, itemDeleteInserts );
 
+		connection.query( itemDeleteSQL, ( error, results, fields ) => {
+			if( error ) return next( error );
+			const successString = `The item ${ID} has been set to inactive`;
+			console.log( successString );
+
+			const dataToReturn = {
+				success: true,
+				data: successString
+			};
+			response.json( dataToReturn );
+		});
+	});
+	server.put( '/api/createlist', ( request, resonse ) => {
+		const { name, description, ownerID, url, securityStatus, dateTime} = request.body;
+
+		const listCreationQuery = 'INSERT INTO lists (??, ??, ??, ??, ??, ??) VALUES (?, ?, ?, ?, ?, ?)';
+		const listCreationInserts = [ 'name', 'description', 'ownerID', 'url', 'securityStatus', 'dateTime', name, description, ownerID, url, securityStatus, dateTime ];
+		const listCreationSQL = mySQL.format( listCreationQuery, listCreationInserts );
+
+		connection.query( listCreationSQL, ( error, results, fields ) => {
+			if( error ) return next( error );
+			const successString = `The list ${name} has been added to the lists table at ${dateTime} by owner ID ${ID}`;
+			console.log( successString );
+
+			const dataToReturn = {
+				success: true,
+				data: successString
+			};
+			response.json( dataToReturn );
+		});
+	});
+	server.patch( '/api/updatelist', ( request, response ) => {
+		const { ID, name, description, ownerID, url, securityStatus, dateTime } = request.body;
+
+		const listUpdateQuery = 'UPDATE lists SET ??=?, ??=?, ??=?, ??=?, ??=?, ??=? WHERE ?? = ?';
+		const listUpdateInserts = [ 'name', 'description', 'ownerID', 'url', 'securityStatus', 'dateTime', name, description, ownerID, url, securityStatus, dateTime, 'ID', ID ];
+		const listUpdateSQL = mySQL.format( listUpdateQuery, listUpdateInserts );
+		
+		connection.query( listUpdateSQL, ( error, results, fields ) => {
+			if( error ) return next( error );
+			const successString = `The list ${ID} has been updated`;
+			console.log( successString );
+
+			const dataToReturn = {
+				success: true,
+				data: successString
+			};
+			response.json( dataToReturn );
+		});
+	});
+	server.delete( '/api/deletelist', ( request, response ) => {
+		const { ID } = request.body;
+
+		const listDeleteQuery = 'UPDATE lists SET ?? = ? WHERE ?? = ?';
+		const listDeleteInserts = [ 'status' , 'inactive' , 'ID', ID ];
+		const listDeleteSQL = mySQL.format( listDeleteQuery, listDeleteInserts );
+
+		connection.query( listDeleteSQL, ( error, results, fields ) => {
+			if( error ) return next( error );
+			const successString = `The list ${ID} has been set to inactive`;
+			console.log( successString );
+
+			const dataToReturn = {
+				success: true,
+				data: successString
+			};
+			response.json( dataToReturn );
+		});
+	});
 }
 
 module.exports = paths;
