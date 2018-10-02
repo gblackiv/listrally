0b2d20bdba25b79e7d52027b17848cf10a2b6ac3
Gerry Blackmon <blackmongerry@gmail.com>
2018-10-01 19:33:43 -0700

    added put and patch methods

--- paths.js
+++ paths.js
@@ -10,7 +10,6 @@ const paths = ( server, mySQL, connection ) => {
 		connection.query( listSQL, ( error, results, fields ) => {
 			if( error ) return next( error );
 	
-			console.log('list results',results);
 			const dataToReturn = {
 				success: true,
 				data: {list: results}
@@ -22,12 +21,50 @@ const paths = ( server, mySQL, connection ) => {
 			connection.query( itemSQL, ( error, results, fields ) => {
 				if( error ) return next( error );
 	
-				console.log( 'item results', results);
 				dataToReturn.data.items = results;
 				response.json( dataToReturn );
 			});
 		});
 	});
+
+	server.put( '/api/newitem', ( request, response ) => {
+		const { name, listID, assignedUserID } = request.body;
+
+		const itemQuery = 'INSERT INTO items ( name, listID, assignedUserID ) VALUES ( ?, ?, ? )';
+		const itemInserts = [ name, listID, assignedUserID ];
+		const itemSQL = mySQL.format( itemQuery, itemInserts );
+
+		connection.query( itemSQL, ( error, results, fields ) => {
+			if( error ) return next( error );
+			console.log( `The item "${name}" has been added to "List ${listID}"` );
+
+			const dataToReturn = {
+				success: true,
+				data: `The item "${name}" has been added to "List ${listID}"`
+			};
+			response.json( dataToReturn );
+		});
+	});
+	server.patch( '/api/updateitem', ( request, response ) => {
+		const { ID, name, listID, assignedUserID } = request.body;
+
+		const itemUpdateQuery = 'UPDATE items SET ??=?, ??=?, ??=? WHERE ?? = ?';
+		const itemUpdateInserts = [ 'name', name, 'listID', listID, 'assignedUserID', assignedUserID, 'ID', ID ];
+		const itemUpdateSQL = mySQL.format( itemUpdateQuery, itemUpdateInserts );
+
+		connection.query( itemUpdateSQL, ( error, resuls, fields ) => {
+			if( error ) return next( error );
+			const successString = `The item ${ID} has been updated to ${name}, ${assignedUserID}`;
+			console.log( successString );
+
+			const dataToReturn = {
+				success: true,
+				data: successString
+			};
+			response.json( dataToReturn );
+		})
+	});
+
 
 }
 
