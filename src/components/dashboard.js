02e25890840a56797d46563e8a2c1a9c209cc385
george-tr6 <george.tr6@gmail.com>
2018-10-02 23:16:57 -0700

    html to react

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,7 +1,69 @@
 import React from 'react';
+import './dashboard.css';
 
 export default props => (    
     <div>
-       <h1>TEMP DASHBOARD PAGE</h1> 
+           <div className="container">
+        {/* <Top Nav */}
+        <div className="nav">
+            <i className="fas fa-chevron-circle-left"></i>
+            <div>Sign Out</div>
+            <i className="fas fa-sign-out-alt"></i>
+        </div>
+        {/* <!-- Main Content --> */}
+        <div className="listContent">
+            {/* <!-- User name, details, user avatar --> */}
+            <div className="top">
+                    <h4 className="details">First Name <br/> Last Name</h4>
+                    {/* <!-- <h4 className="details">Last Name</h4> --> */}
+
+                    <i className="far fa-user"></i>
+                    
+            </div>
+            {/* <!-- List Toggle --> */}
+            <div className="list_toggle">
+                    <div>All Lists</div>
+                    <i className="fas fa-toggle-off"></i>
+                    <button className="button">User Settings</button>
+                </div>
+
+            {/* <!-- Items --> */}
+            <div className="items">
+                <div className="item1">
+                    <div className="left">
+                        <i className="fas fa-link"></i>
+                        My First List
+                    </div>
+                    <div className="right">
+                        <i className="fas fa-unlock-alt"></i>
+                        <i className="fas fa-cog"></i>
+                    </div>
+                </div>
+                <div className="item2">
+                    <div className="left">
+                        <i className="fas fa-link"></i>
+                        Office Potluck List
+                    </div>
+                    <div className="right">
+                        <i className="fas fa-unlock-alt"></i>
+                        <i className="fas fa-cog"></i>
+                    </div>
+                </div>
+                <div className="item2">
+                    <div className="left">
+                        <i className="fas fa-link"></i>
+                        Family Reunion
+                    </div>
+                    <div className="right">
+                        <i className="fas fa-unlock-alt"></i>
+                        <i className="fas fa-cog"></i>
+                    </div>
+                </div> 
+            </div>
+            <br/>
+            {/* <!-- Add List Button --> */}
+            <i className="fas fa-plus-circle"></i>
+        </div>
+    </div>
     </div>
 )
