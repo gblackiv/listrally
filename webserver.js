0fe1d3d9d54fa4a198c15e0670e169e9a9df07cf
Gerry Blackmon <blackmongerry@gmail.com>
2018-10-03 17:33:16 -0700

    initial login with google made

--- webserver.js
+++ webserver.js
@@ -1,9 +1,13 @@
 const express = require( 'express' );
 const mySQL = require( 'mysql' );
+const passport = require( 'passport' );
+const passportSetup = require( './config/passportSetup.js' );
+const mysqlCredentials = require( './config/mySQLCredentials.js' );
+const paths = require( './paths' );
+const keys = require( './config/keys.js' );
+const cookieSession = require( 'cookie-session' );
 const server = express();
 const PORT = 3050;
-const mysqlCredentials = require( './mySQLCredentials.js' );
-const paths = require( './paths' );
 
 const connection = mySQL.createConnection( mysqlCredentials );
 connection.connect( error => {
@@ -12,6 +16,12 @@ connection.connect( error => {
 	console.log( `connected to ${mysqlCredentials.database}` );
 });
 
+server.use( cookieSession({
+	maxAge: 24 * 60 * 60 * 1000,	//one day in milliseconds
+	keys: keys.cookieKey
+}));
+server.use( passport.initialize() );
+server.use( passport.session() );
 server.use( ( request, response, next ) => {
 	response.header( "Access-Control-Allow-Origin", "*" );
 	response.header( "Access-Control-Allow-Headers", "Origin, X-Request-With, Content-Type, Accept" );
@@ -22,6 +32,17 @@ server.use( express.urlencoded() );
 server.use( express.static( `${__dirname}/client/dist` ) );
 
 paths( server, mySQL, connection );
+passportSetup( server, mySQL, connection, passport );
+server.get( '/auth/login', passport.authenticate( 'google', {
+	scope: [ 'profile' ],
+	access_type: 'offline'
+	})
+);
+server.get( '/auth/login/redirect', passport.authenticate( 'google' ), ( request, response ) => {
+	response.send('callback URI reached!')
+});
+
+
 
 
 server.listen( PORT, () => { console.log( `server is listening on port ${PORT}` ) } );
