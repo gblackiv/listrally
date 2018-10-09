b3f0b865f16d1d5ef68385319554d71f8db5e4f7
Michael Salvati <print@3daddict.com>
2018-10-08 19:38:51 -0700

    working on lock buttons components

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -3,27 +3,28 @@ import { connect } from 'react-redux';
 import '../assets/css/home.scss';
 import logo from '../assets/images/todolist.png';
 import newList from '../assets/images/new-list-white.png';
-import google from '../assets/images/btn_google_signin_dark_normal_web.png'
 import { Link } from 'react-router-dom';
+import ListButton from './buttons/list_button';
+import SignInButton from './buttons/sign_in_button';
 
 
 
 export default class Home extends Component{
     render(){
-        return     (    
+        return (    
             <div>
                 <div className="home-container">
                     <div className="home-content">
                         <img id="logo" src={logo} alt="logo"/>
                         <div className="home-title">ListRally</div>
-                        <img src={google} alt="" className="sign-in"/>
+                        <SignInButton />
                         <div className="new-list">
                             <p className="instruction">Click the + icon to make a list</p>
                             <Link to="/list"><img className="new-list-btn" src={newList} alt="new_list"/></Link>
                         </div>
                         <div className="home-templates">
                             <p className="instruction">or click the arrow to select a list template</p>
-                            <i className="home-arrow fas fa-angle-down"></i>
+                                <ListButton />
                             <div>
                                 <div className="home-hidden template">Office potluck list</div>
                                 <div className="home-hidden template">BBQ at the park lIst</div>
