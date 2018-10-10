8b7008e92fe32507e664064c3ce9cd6988685c3d
Gerry Blackmon <blackmongerry@gmail.com>
2018-10-10 03:53:42 -0700

    added more buttons to choose from

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -28,33 +28,3 @@ export default class Header extends React.Component {
     }
 }
 
-
-// const components = {
-//     list_button,
-//     list_link_button
-// }
-
-
-// export default ( props ) => {
-//     //const DynamicButton = components[props.buttonType];
-//     console.log(props.button)
-//     console.log( components[ props.button])
-//     const FirstButton = React.createElement( props.button );
-//         return (
-//         <div>
-//             <div className="dashboard-nav">
-//             <FirstButton />
-//                 {/* <DynamicButton button={props.button[0]} />
-//                 <DynamicButton button={props.button[1]} />  */}
-                
-//             </div> 
-//         </div>
-//     )
-// }
-
-
-                // <div className="dashboard-nav">
-                //     <i className="far fa-chevron-circle-left"></i>
-                //     <div className="dashboard-signout">Sign Out</div>
-                //     <i className="far fa-sign-out-alt"></i>   
-                // </div>
