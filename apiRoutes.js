bf8f98f3745f2393201ca8c07a002fda0e2333eb
gblackiv <blackmongerry@gmail.com>
2018-10-11 14:00:34 -0700

    user verification for item update complete

--- apiRoutes.js
+++ apiRoutes.js
@@ -96,29 +96,70 @@ const paths = ( server, mySQL, connection ) => {
 	 */
 	server.patch( '/api/updateitem', ( request, response ) => {
 		const { ID, name, listID, assignedUserID } = request.body;
+		if(!ID || !name || !listID || !assignedUserID){
+			const dataToReturn = {
+				success: false,
+				data: 'missing item update information'
+			};
+			response.json( dataToReturn );
+			return;
+		}
+		const itemUserVerificationQuery = 'SELECT * FROM ?? WHERE ?? = ?';
+		const itemUserVerificationInserts = ['items', 'ID', ID];
+		const itemUserVerificationSQL = mySQL.format( itemUserVerificationQuery, itemUserVerificationInserts );
 
-		const itemUpdateQuery = 'UPDATE items SET ??=?, ??=?, ??=? WHERE ?? = ?';
-		const itemUpdateInserts = [ 'name', name, 'listID', listID, 'assignedUserID', assignedUserID, 'ID', ID ];
-		const itemUpdateSQL = mySQL.format( itemUpdateQuery, itemUpdateInserts );
-
-		connection.query( itemUpdateSQL, ( error, results, fields ) => {
-			if( error ){		//missing data for the item to be updated
-				console.log( "/api/updateitem Error:", error );
+		connection.query( itemUserVerificationSQL, ( error, results, fields ) => {
+			if( error ){		//the itemID that was trying to be deleted was incorrect
+				console.log( '/api/updateitem error:', error );
 				const dataToReturn = {
 					success: false,
-					data: "Error: did not receive the expected items fields"
+					data: "Error: could not find item with the requested ID"
 				}
 				response.json( dataToReturn );
 				return;
-			};
-			const successString = `The item ${ID} has been updated`;
-			console.log( successString );
+			}
 
-			const dataToReturn = {
-				success: true,
-				data: successString
-			};
-			response.json( dataToReturn );
+			if( request.user.ID !== results[0].assignedUserID ){
+				console.log( '/api/updateitem issue: unauthorized user attemped to update item ID', ID);
+				const dataToReturn = {
+					success: false,
+					data: 'Error: user is unauthorized to update the selected item'
+				};
+				response.json( dataToReturn );
+				return;
+			}
+			let changedFields = '';
+			if(results[0].name !== name){
+				changedFields += 'name was changed ';
+			}
+			if(results[0].assignedUserID != assignedUserID){
+				changedFields += 'assigned user was changed ';
+			}
+			const itemUpdateQuery = 'UPDATE items SET ??=?, ??=?, ??=? WHERE ?? = ?';
+			const itemUpdateInserts = [ 'name', name, 'listID', listID, 'assignedUserID', assignedUserID, 'ID', ID ];
+			const itemUpdateSQL = mySQL.format( itemUpdateQuery, itemUpdateInserts );
+			console.log(itemUpdateSQL);
+
+			connection.query( itemUpdateSQL, ( error, results, fields ) => {
+				if( error ){		//missing data for the item to be updated
+					console.log( "/api/updateitem Error:", error );
+					const dataToReturn = {
+						success: false,
+						data: "Error: did not receive the expected items fields"
+					}
+					response.json( dataToReturn );
+					return;
+				};
+				const successString = `The item ${ID} has been updated`;
+				console.log( successString );
+
+				const dataToReturn = {
+					success: true,
+					data: successString,
+					changedFields
+				};
+				response.json( dataToReturn );
+			});
 		});
 	});
 	/**
