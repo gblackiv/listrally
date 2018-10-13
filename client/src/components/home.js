da33d93bacc31f69d9ec6bda564a3544411a3750
Michael Salvati <print@3daddict.com>
2018-10-12 18:21:36 -0700

    homepage converted to flex layout, icon updated and toggle list completed

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,66 +1,82 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import '../assets/css/home.scss';
-import logo from '../assets/images/todolist.png';
+import logo from '../assets/images/app-list-rally-logo-icon-BLUE.png';
 import newList from '../assets/images/new-list-white.png';
 import { Link } from 'react-router-dom';
 import ListButton from './buttons/list_button';
 import SignInButton from './buttons/sign_in_button';
 import { authenticate } from '../actions';
-
-
+import Header from './header';
+import { Fragment } from 'react'
 
 class Home extends Component{
-
-    login = ()=>{
-        debugger;
-        this.props.authenticate();
+    constructor(props) {
+		super(props);
+		this.state = {
+            show: false
+        };
+        this.toggleListVisibility = this.toggleListVisibility.bind(this);
     }
 
+    toggleListVisibility = () => {
+        console.log('clicked')
+        const { show } = this.state;
+        this.setState( { show: !show } )
+
+    }
 
     render(){
-        return (    
-            
-            <div>
-                <div className="home-container">
-                    <div className="home-content">
-                        <img onClick={this.login} id="logo" src={logo} alt="logo"/>
+        return(
+            <div className="col-2">
+            <header>
+                <Header buttons={[]}/>
+            </header> 
+                <div className='content'>
+                    <div className="home-container">
+                        <div className="home-content">
+                        <div className="logo-container">
+                            <img className="shadow" onClick={this.login} id="logo" src={logo} alt="logo"/>
+                        </div>
                         <div className="home-title">ListRally</div>
-                        {/* < SignInButton onClick={this.login}  /> */}
-                        <button onClick={this.login}  className="login">Login</button>
-                        <div className="new-list">
-                            <p className="instruction">Click the + icon to make a list</p>
-                            <Link to="/list"><img className="new-list-btn" src={newList} alt="new_list"/></Link>
-                        </div>
-                        <div className="home-templates">
-                            <p className="instruction">or click the arrow to select a list template</p>
-                                <ListButton />
-                            <div>
-                                <div className="home-hidden template">Office potluck list</div>
-                                <div className="home-hidden template">BBQ at the park lIst</div>
-                                <div className="home-hidden template">A day at the beach list</div>
-                                <div className="home-hidden template">Vegan dinner party list<div/>
+                                <SignInButton className="login" onClick={this.login}  />
+                            <div className="new-list">
+                                <p className="instruction home-text">Click the + icon to make a list</p>
+                                <Link to="/list"><img className="new-list-btn" src={newList} alt="new_list"/></Link>
                             </div>
-                        </div>
+                            <div className="home-templates">
+                                <p className="instruction home-text">or click the arrow to select a list template</p>
+                                    <div  onClick={ this.toggleListVisibility }><ListButton /></div>
+                                <div className="home-text">
+                                    {this.state.show && <Box />}
+                                </div>
+                            </div>
                     </div>
+        
                     </div>
                 </div>
+                <footer>
+                <p>Footer Component Here</p>
+            </footer>
             </div>
         )
     }
 }
 
-// function mapStateToProps(state){
-//     console.log('Redux state list inside mapStateToProp :', state.list.list);
-//     console.log('Redux state items inside mapStateToProp :', state.list.items);
-//     return {
-//         list: state.list.list,
-//         items: state.list.items
-//     }
-// }
+class Box extends Component {
+    render(){
+        return(
+            <Fragment>
+                <div className="template">Office potluck list</div>
+                <div className="template">BBQ at the park list</div>
+                <div className="template">A day at the beach list</div>
+                <div className="template">Vegan dinner party list</div>
+            </Fragment>
+        )
+    }
+}
 
 
 export default connect(null,{
     authenticate
-})(Home); 
-
+})(Home);
