7398d033ee24172c2918a53b8eded3fd8cbc7723
Michael Salvati <print@3daddict.com>
2018-10-16 16:28:05 -0700

    img hover color css

---
+++ client/src/components/about.1.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import '../assets/css/about.css';
+import michael from '../assets/images/michael.jpeg';
+import gerry from '../assets/images/gerry-bw.jpg';
+import george from '../assets/images/george-bw.jpg';
+import torie from '../assets/images/torie-bw.jpg';
+import Header from './header';
+
+export default props =>(
+    <div className="col-2">
+        <header>
+            <Header buttons={['Back_button']}/>
+        </header> 
+        <div className='content'>
+            <div className="layout-container">
+                <div className="about-container">
+                    <h3>About</h3>
+                    <div className="about-team">
+                        <div className="about-person michael">
+                            <img className="member" src={michael}/>
+                            <div className="member-name">Michael</div>
+                            <div className="about-role">Front-End</div>
+                            <div className="about-media">
+                                <i className="fab fa-github"></i>
+                                <i className="fab fa-linkedin"></i>
+                            </div>
+                        </div>
+                        <div className="about-person george">
+                            <img className="member" src={george} alt="george"/>
+                            <div className="member-name">George</div>
+                            <div className="about-role">Front-End</div>
+                            <div className="about-media">
+                                <i className="fab fa-github"></i>
+                                <i className="fab fa-linkedin"></i>
+                            </div>
+                        </div>
+                        <div className="about-person torie">
+                            <img className="member" src={torie} alt="torie"/>
+                            <div className="member-name">Torie</div>
+                            <div className="about-role">Front-End</div>
+                            <div className="about-media">
+                                <i className="fab fa-github"></i>
+                                <i className="fab fa-linkedin"></i>
+                            </div>
+                        </div>
+                        <div className="about-person gerry">
+                            <img className="member" src={gerry} alt="gerry"/>
+                            <div className="member-name">Gerry</div>
+                            <div className="about-role">Back-End</div>
+                            <div className="about-media">
+                                <i className="fab fa-github"></i>
+                                <i className="fab fa-linkedin"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <footer>
+            <p>Footer Component Here</p>
+        </footer>
+    </div>
+)
--- client/src/components/about.js
+++ client/src/components/about.js
@@ -1,63 +1,121 @@
-import React from 'react';
-import '../assets/css/about.css';
-import michael from '../assets/images/michael.jpeg';
-import gerry from '../assets/images/gerry-bw.jpg';
-import george from '../assets/images/george-bw.jpg';
-import torie from '../assets/images/torie-bw.jpg';
+import React, { Component } from 'react';
+import '../assets/css/about.scss';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import Header from './header';
+import Footer from './footer';
 
-export default props =>(
-    <div className="col-2">
-        <header>
-            <Header buttons={['Back_button']}/>
-        </header> 
-        <div className='content'>
-            <div className="layout-container">
-                <div className="about-container">
-                    <h3>About</h3>
-                    <div className="about-team">
-                        <div className="about-person michael">
-                            <img className="member" src={michael}/>
-                            <div className="member-name">Michael</div>
-                            <div className="about-role">Front-End</div>
-                            <div className="about-media">
-                                <i className="fab fa-github"></i>
-                                <i className="fab fa-linkedin"></i>
+import WhiteLogo from '../assets/images/aboutus/app-list-rally-logo-icon-ONLY-LIGHT.png'
+import michael from '../assets/images/aboutus/michael.png';
+import gerry from '../assets/images/aboutus/gerry.png';
+import george from '../assets/images/aboutus/george.png';
+import torie from '../assets/images/aboutus/torie.png';
+
+
+
+// import '../../node_modules/flatpickr/dist/themes/airbnb.css'
+// import Flatpickr from 'react-flatpickr'
+
+class AboutUs extends Component{
+    constructor() {
+        super();
+     
+    this.state = {};
+    }
+
+    render(){
+
+        return(
+            <div className="col-2">
+                <header>
+                    <Header buttons={['Back_button', 'Home_nav_button']} history={this.props.history} />
+                </header> 
+                <div className='content'>
+                    <div className="layout-container">
+                        <div className="hero-container">
+                            <div className="banner-container">
+                            <img className="about-white-logo shadow" src={WhiteLogo} alt="list" />
+                            <h1 className="about-header shadow">ListRally Team</h1>
                             </div>
                         </div>
-                        <div className="about-person george">
-                            <img className="member" src={george} alt="george"/>
-                            <div className="member-name">George</div>
-                            <div className="about-role">Front-End</div>
-                            <div className="about-media">
-                                <i className="fab fa-github"></i>
-                                <i className="fab fa-linkedin"></i>
-                            </div>
+                        <div className="text-container">
+                        <h4 className="team-text">Meet the Team</h4>
                         </div>
-                        <div className="about-person torie">
-                            <img className="member" src={torie} alt="torie"/>
-                            <div className="member-name">Torie</div>
-                            <div className="about-role">Front-End</div>
-                            <div className="about-media">
-                                <i className="fab fa-github"></i>
-                                <i className="fab fa-linkedin"></i>
+                        <p className="about-p">A group a creative web developers</p>
+                        <div className="team-wrapper">
+                            <div className="team-container">
+                                <div className="team-card">
+                                    <div className="team-img-container">
+                                        <img className="team-img shadow" src={michael} alt="michael_photo" />
+                                    </div>
+                                    <h6 className="team-title">Michael</h6>
+                                    <p className="team-role">Front-End Developer</p>
+                                    <div className="team-icons">
+                                        <a href="https://github.com/3daddict" target="_blank"><i className="fab fa-github"></i></a> 
+                                        <a href="https://www.linkedin.com/in/msalvati/" target="_blank"><i className="fab fa-linkedin"></i></a>
+                                        <a href="http://mikedoescode.com" target="_blank"><i className="fal fa-home"></i></a>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                        <div className="about-person gerry">
-                            <img className="member" src={gerry} alt="gerry"/>
-                            <div className="member-name">Gerry</div>
-                            <div className="about-role">Back-End</div>
-                            <div className="about-media">
-                                <i className="fab fa-github"></i>
-                                <i className="fab fa-linkedin"></i>
+                            <div className="team-container">
+                                <div className="team-card">
+                                    <div className="team-img-container">
+                                        <img className="team-img shadow" src={george} alt="michael_photo" />
+                                    </div>
+                                    <h6 className="team-title">George</h6>
+                                    <p className="team-role">Front-End Developer</p>
+                                    <div className="team-icons">
+                                        <a href="https://github.com/george-tr6" target="_blank"><i className="fab fa-github"></i></a> 
+                                        <a href="https://www.linkedin.com/in/george-trendafilov-57327514/" target="_blank"><i className="fab fa-linkedin"></i></a>
+                                        <a href="http://georgetrendafilov.com" target="_blank"><i className="fal fa-home"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="team-container">
+                                <div className="team-card">
+                                    <div className="team-img-container">
+                                        <img className="team-img shadow" src={torie} alt="michael_photo" />
+                                    </div>
+                                    <h6 className="team-title">Torie</h6>
+                                    <p className="team-role">Front-End Developer</p>
+                                    <div className="team-icons">
+                                        <a href="https://github.com/toriep" target="_blank"><i className="fab fa-github"></i></a> 
+                                        <a href="https://www.linkedin.com/in/torie-pham-6476ab79/" target="_blank"><i className="fab fa-linkedin"></i></a>
+                                        <a href="http://toriepham.com/main.html" target="_blank"><i className="fal fa-home"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="team-container">
+                                <div className="team-card">
+                                    <div className="team-img-container">
+                                        <img className="team-img shadow" src={gerry} alt="michael_photo" />
+                                    </div>
+                                    <h6 className="team-title">Gerry</h6>
+                                    <p className="team-role">Back-End Developer</p>
+                                    <div className="team-icons">
+                                        <a href="https://github.com/gblackiv" target="_blank"><i className="fab fa-github"></i></a> 
+                                        <a href="https://www.linkedin.com/in/gerry-blackmon-b20b98165/" target="_blank"><i className="fab fa-linkedin"></i></a>
+                                        <a href="http://gerryblackmon.com/" target="_blank"><i className="fal fa-home"></i></a>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
+                <footer>
+                    <Link to={`/list/${this.props.url}`}><Footer buttons={['next_page_button']} /></Link>
+                </footer>
             </div>
-        </div>
-        <footer>
-            <p>Footer Component Here</p>
-        </footer>
-    </div>
-)
+        )
+    }
+}
+
+function mapStateToProps(state){
+    return {
+        
+    }
+}
+
+export default connect(mapStateToProps,{
+    
+})(AboutUs); 
