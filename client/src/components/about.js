c10be09b5873e2d1215b8e6c6870711fbece9390
Michael Salvati <print@3daddict.com>
2018-10-18 16:18:21 -0700

    about page updated and photos added

--- client/src/components/about.js
+++ client/src/components/about.js
@@ -11,11 +11,6 @@ import gerry from '../assets/images/aboutus/gerry.png';
 import george from '../assets/images/aboutus/george.png';
 import torie from '../assets/images/aboutus/torie.png';
 
-
-
-// import '../../node_modules/flatpickr/dist/themes/airbnb.css'
-// import Flatpickr from 'react-flatpickr'
-
 class AboutUs extends Component{
     constructor() {
         super();
@@ -99,7 +94,18 @@ class AboutUs extends Component{
                                     </div>
                                 </div>
                             </div>
+                                <div className="about-app-container">
+                                    <h4 className="team-text">About the ListRally App</h4>
+                                </div>
+                                <div className="about-text">
+                                <p>The ListRally App was created to solve the problem of hosting an event and people asking what they can bring. We wanted to create an easy and shareable list, so groups can collaborate on items needed for various occasions.</p>
+                                <p>Starting with feature set planning for an MVP (Minimum Viable Product), we planned out the pages and features we wanted to incorporate into the app. Utilizing <strong>Figma</strong> we created some concepts and started planning the <strong>Agile Process</strong> using <strong>Trello</strong>.</p>
+                                <p>The ListRally App is built with a <strong>NodeJS</strong> Back-End and a <strong>ReactJS</strong> Front-End. The Back-End features a <strong>mySQL Database</strong> with Authentication and Error Report Handling. It also features an <strong>PHP Mailer</strong> for email notifications of upcoming events.</p>
+                                <p>The Front-End is using <strong>Redux</strong> which is a Framework that handles state to behave consistently, run in different environments. Along with Redux we also used <strong>React Router DOM, Redux Form, Sass </strong>and other technologies.</p>
+                                <p>Thank you for checkout out our app! We hope you enjoy it as much as we did making it.</p>
+                                </div>
                         </div>
+                        
                     </div>
                 </div>
                 <footer>
