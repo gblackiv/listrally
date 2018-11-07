3ffeb4403d4c95e5b5e007bdb04bfac4267e0f7c
gblackiv <blackmongerry@gmail.com>
2018-11-07 12:58:20 -0800

    added spinner to dashboard

--- client/src/components/loading_spinner.js
+++ client/src/components/loading_spinner.js
@@ -2,8 +2,7 @@ import React from 'react';
 import '../assets/css/loading_spinner.css';
 
 
-export default (props) => {
-	<div className="lds-ring"><div></div><div></div><div></div><div></div></div>
-}
+export default (props) => <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
+
 
 //credit to https://loading.io/css/ for the spinner css and html
